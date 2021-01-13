ec5bcf2144c66db8d50120e31988a70a6ed303f3
adiwon <aditya.rathi@vedantu.com>
2021-01-13 22:04:11 +0530

    nav complete

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/FragmantGallery.kt
@@ -0,0 +1,7 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.codinginflow.imagesearchapp.R
+
+class FragmantGallery : Fragment(R.layout.fragmant_gallery) {
+}
